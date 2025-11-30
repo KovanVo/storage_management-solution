3e2602fd25817040cf7ab77aed33e04ed97f5a42
BubbleThy12 <unicornvo123@gmail.com>
2025-11-30 11:04:00 +1100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB"
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'syd.cloud.appwrite.io',
+        pathname: '/v1/storage/buckets/**',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
